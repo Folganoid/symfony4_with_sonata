cce36db9e8b7f856293aac11044ec1cfded659ef
Andrey <folganoid@yahoo.com>
2018-05-06 21:17:54 +0300

    deploy to docker

---
+++ start.sh
@@ -0,0 +1,8 @@
+docker-compose up -d
+echo waiting 30 sec - do not ask why
+sleep 30
+docker-compose exec -d php-fpm php bin/console make:migration &&#
+docker-compose exec -d php-fpm php bin/console doctrine:migrations:migrate -q &&#
+docker-compose exec -d php-fpm php bin/console doctrine:schema:update --force &&#
+docker-compose exec -d php-fpm php bin/console fos:user:create admin admin@admin.com admin --super-admin
+echo localhost:8000/admin
